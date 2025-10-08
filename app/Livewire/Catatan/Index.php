4cea015a5db48d9a1485160b5510ecbeb396cb73
w1y0n0 <wiyono30@gmail.com>
2025-10-08 22:01:12 +0700

    Konfigurasi Index Class Component Livewire

--- app/Livewire/Catatan/Index.php
+++ app/Livewire/Catatan/Index.php
@@ -2,12 +2,40 @@
 
 namespace App\Livewire\Catatan;
 
+use App\Models\Catatan;
+use Flux\Flux;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+    public $catatanId;
+
+    public function edit($id)
+    {
+        $this->dispatch('edit-catatan', $id);
+    }
+
     public function render()
     {
-        return view('livewire.catatan.index');
+        $catatans = Catatan::orderBy('created_at', 'desc')->paginate(5);
+
+        return view('livewire.catatan.index', [
+            'catatans' => $catatans,
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $this->catatanId = $id;
+        Flux::modal('delete-catatan')->show();
+    }
+
+    public function deleteCatatan()
+    {
+        Catatan::findOrFail($this->catatanId)->delete();
+        Flux::modal('delete-catatan')->close();
+        session()->flash('success', 'Catatan berhasil dihapus.');
     }
 }
