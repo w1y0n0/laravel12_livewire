90ea150cd40471e5cc95a5a2bc883009b6b40a76
w1y0n0 <wiyono30@gmail.com>
2025-10-09 22:13:42 +0700

    Mengatur Route di Laravel 12 Livewire

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Catatan\Index;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -14,6 +15,10 @@ Route::get('/', function () {
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
+
+Route::get('catatans', Index::class)
+    ->middleware(['auth', 'verified'])
+    ->name('catatans');
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
