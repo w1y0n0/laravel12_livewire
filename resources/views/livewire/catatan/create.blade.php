2f99d88af4567a2d0725034335b4ccf7eb80ec9d
w1y0n0 <wiyono30@gmail.com>
2025-10-10 15:52:34 +0700

    Create Livewire Blade View

--- resources/views/livewire/catatan/create.blade.php
+++ resources/views/livewire/catatan/create.blade.php
@@ -1,3 +1,20 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+    <flux:modal name="create-catatan" class="md:w-900">
+        <div class="space-y-6">
+            <div>
+                <flux:heading size="lg">Create Catatan</flux:heading>
+                <flux:text class="mt-2">Make changes to your catatan details.</flux:text>
+            </div>
+
+            <flux:input label="Judul" wire:model="judul" placeholder="Masukkan Judul Catatan" />
+
+            <flux:textarea label="Isi" wire:model="isi" placeholder="Masukkan Isi Catatan" />
+
+            <div class="flex">
+                <flux:spacer />
+
+                <flux:button type="submit" variant="primary" wire:click="save">Save changes</flux:button>
+            </div>
+        </div>
+    </flux:modal>
 </div>
